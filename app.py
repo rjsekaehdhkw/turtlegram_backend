c3345a8fbd0f33e844a4d1a15d6f7f2a7c0283d0
Minjae <rjsekaehdhkw@gmail.com>
2022-05-17 02:49:46 +0900

    프로젝트 하는중

---
+++ app.py
@@ -0,0 +1,25 @@
+import json
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
+
+
+@app.route('/')
+def hello_world():
+    return jsonify({'message': 'success'})
+
+
+@app.route("/signup", methods=['POST'])
+def sign_up():
+    data = json.loads(request.data)
+    print(data.get('email'))
+    print(data["password"])
+
+    return jsonify({'message': 'success2'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5002, debug=True)
